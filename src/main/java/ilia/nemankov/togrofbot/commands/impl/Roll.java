7a39f497f063ea87237e27819fa7f4d6415231d4
SpeedWagon <ilia.nemankov@gmail.com>
2019-08-12 11:28:37 +0300

    Added Roll command item with custom number range borders

--- src/main/java/ilia/nemankov/togrofbot/commands/impl/Roll.java
+++ src/main/java/ilia/nemankov/togrofbot/commands/impl/Roll.java
@@ -4,7 +4,9 @@ import ilia.nemankov.togrofbot.commands.AbstractCommand;
 import ilia.nemankov.togrofbot.commands.CommandItem;
 import ilia.nemankov.togrofbot.commands.parsing.CommandVariantDescription;
 import ilia.nemankov.togrofbot.commands.parsing.argument.Argument;
+import ilia.nemankov.togrofbot.commands.parsing.argument.NumberArgument;
 import ilia.nemankov.togrofbot.commands.parsing.matching.ArgumentsTemplate;
+import ilia.nemankov.togrofbot.commands.parsing.matching.NumberArgumentMatcher;
 import ilia.nemankov.togrofbot.settings.SettingsProvider;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import org.slf4j.Logger;
@@ -30,6 +32,7 @@ public class Roll extends AbstractCommand {
     public Roll() {
         List<CommandItem> commandItems = new ArrayList<>();
         commandItems.add(new DefaultRoll());
+        commandItems.add(new BorderedRoll());
         setCommandItems(commandItems);
     }
 
@@ -58,6 +61,47 @@ public class Roll extends AbstractCommand {
                     resources.getString("message.command.roll.result"),
                     event.getAuthor().getAsMention(),
                     random.nextInt(101)
+            );
+        }
+    }
+
+    private class BorderedRoll extends CommandItem {
+
+        public BorderedRoll() {
+            super(new ArgumentsTemplate(null, new NumberArgumentMatcher(), new NumberArgumentMatcher()));
+        }
+
+        @Override
+        public CommandVariantDescription getDescription() {
+            ResourceBundle resources = ResourceBundle.getBundle("lang.lang", SettingsProvider.getInstance().getLocale());
+            CommandVariantDescription description = new CommandVariantDescription(
+                    resources.getString("description.command.roll.bordered.args"),
+                    resources.getString("description.command.roll.bordered.desc")
+            );
+            return description;
+        }
+
+        @Override
+        public String execute(GuildMessageReceivedEvent event, List<Argument> arguments) {
+            ResourceBundle resources = ResourceBundle.getBundle("lang.lang", SettingsProvider.getInstance().getLocale());
+
+            int bottomBorder = ((NumberArgument)arguments.get(0)).getNumberArgument().intValue();
+            if (bottomBorder < 0) {
+                return resources.getString("message.command.roll.negative_bottom_border");
+            }
+            int topBorder = ((NumberArgument)arguments.get(1)).getNumberArgument().intValue();
+            if (topBorder < 0) {
+                return resources.getString("message.command.roll.negative_top_border");
+            }
+            if (topBorder < bottomBorder) {
+                return resources.getString("message.command.roll.incorrect_borders");
+            }
+
+            Random random = new Random(System.currentTimeMillis());
+            return MessageFormat.format(
+                    resources.getString("message.command.roll.result"),
+                    event.getAuthor().getAsMention(),
+                    (random.nextInt(topBorder - bottomBorder + 1) + bottomBorder)
             );
         }
     }
